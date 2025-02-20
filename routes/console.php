619bccdbd9cf22894ce66fa7a9217f33f1128b49
Marco <mark_o002@hotmail.com>
2025-02-19 23:49:52 -0600

    config upd

--- routes/console.php
+++ routes/console.php
@@ -12,4 +12,4 @@ Artisan::command('inspire', function () {
 
 Schedule::command('app:create-random-orders-command')->everySecond();
 
-Schedule::job(new CreateRandomOrdersJob)->everyTwoMinutes();
+Schedule::job(new CreateRandomOrdersJob)->everyTwoSeconds();
