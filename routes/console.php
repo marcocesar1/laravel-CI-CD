39726a02791fe803f9e5a130becfeba1f50b8ff3
Marco <mark_o002@hotmail.com>
2025-02-21 00:13:01 -0600

    cron and jobs

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:test-command')->everyTenMinutes();
+Schedule::command('app:test-command')->everyMinute();
 
 Schedule::job(new CreateRandomOrdersJob)->hourly();
